7d4669eea24b0cc28c6e7f960f275eb24df19ba7
Mikonu <djkehh@naver.com>
2024-10-19 05:40:08 +0900

    #30 - 게시글 추가, 수정, 삭제 기능 구현
    게시글을 추가하고 수정하는 화면은 동일한 뷰를 쓸 예정이라,
    구분을 위한 `FormStatus`를 도입.
    새로운 이름의 enum을 도입하여
    알맞게 패키지명을 새로 고민함.
    
    서비스는 게시글 세부 기능 구현 요구사항에 맞춰
    필요한 부분을 수정하고,
    회원 정보가 올바르게 전달되게끔 하고,
    게시글만 필요한 경우와 댓글을 함께 가져와야 하는 경우를
    기능으로 나눔.

--- src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
@@ -1,6 +1,7 @@
 package com.fastcampus.projectboard.dto;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.UserAccount;
 
 import java.time.LocalDateTime;
 
@@ -38,9 +39,9 @@ public record ArticleDto(
         );
     }
 
-    public Article toEntity() {
+    public Article toEntity(UserAccount userAccount) {
         return Article.of(
-                userAccountDto.toEntity(),
+                userAccount,
                 title,
                 content,
                 hashtag
